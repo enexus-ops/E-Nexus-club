65a77c6b3db173dca4a0bb671b82588e03dea22e
megha-r20 <megha202005@gmail.com>
2025-10-02 12:40:46 +0530

    Add event page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import AboutENexus from './components/About/AboutENexus'; 
+import EventsPage from './components/Event/EventsPage';
 import React from "react";
 import "./App.css"; 
 import EnexusLanding from "./components/EnexusLanding";
@@ -14,6 +15,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           {/* ✅ Fixed the component name */}
           <Route path="/About" element={<AboutENexus />} />
+          <Route path="/events" element={<EventsPage />} /> 
         </Routes>
       </Router>
     </>
